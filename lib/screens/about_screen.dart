978bb63d7773370a66d933d0593d9b8d56a12310
Prateek Kumar Oraon <b170078@nitsikkim.ac.in>
2020-04-20 19:42:00 +0530

    Bug fixing

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -6,7 +6,6 @@ import 'package:covid19_tracker/utilities/network_handler.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'package:url_launcher/url_launcher.dart' as browser;
 
 class AboutScreen extends StatefulWidget{
   @override
@@ -262,41 +261,36 @@ class _AboutScreenState extends State<AboutScreen>{
                           ],
                         ),
                       ),
-                      InkWell(
-                        onTap: (){
-                          _networkHandler.launchInBrowser(developerPrateekGitHubLink);
-                        },
-                        child: Container(
-                          height: 56,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              SizedBox(width: 30,),
-                              Icon(
-                                darkTheme?Icons.brightness_2:Icons.brightness_5,
-                                color: theme.accentColor,
-                              ),
-                              SizedBox(width: 30,),
-                              Expanded(
-                                child: Text(
-                                  'Dark Theme',
-                                  style: TextStyle(
-                                    fontSize: 16*textScaleFactor,
-                                  ),
+                      Container(
+                        height: 56,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(width: 30,),
+                            Icon(
+                              darkTheme?Icons.brightness_2:Icons.brightness_5,
+                              color: theme.accentColor,
+                            ),
+                            SizedBox(width: 30,),
+                            Expanded(
+                              child: Text(
+                                darkTheme?'Light Theme':'Dark Theme',
+                                style: TextStyle(
+                                  fontSize: 16*textScaleFactor,
                                 ),
                               ),
-                              Switch(
-                                value: darkTheme,
-                                activeColor: theme.accentColor,
-                                onChanged: (bool value){
-                                  changeBrightness();
-                                  setState(() {
-                                    darkTheme = value;
-                                  });
-                                },
-                              )
-                            ],
-                          ),
+                            ),
+                            Switch(
+                              value: darkTheme,
+                              activeColor: theme.accentColor,
+                              onChanged: (bool value){
+                                changeBrightness();
+                                setState(() {
+                                  darkTheme = value;
+                                });
+                              },
+                            )
+                          ],
                         ),
                       ),
                     ],
@@ -326,33 +320,6 @@ class _AboutScreenState extends State<AboutScreen>{
                           fontSize: 25*textScaleFactor,
                           fontFamily: kQuickSand,
                           fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      InkWell(
-                        onTap: (){
-                          //TODO
-                        },
-                        child: Container(
-                          height: 56,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              SizedBox(width: 30,),
-                              Icon(
-                                AntDesign.infocirlceo,
-                                color: theme.accentColor,
-                              ),
-                              SizedBox(width: 30,),
-                              Expanded(
-                                child: Text(
-                                  'Application',
-                                  style: TextStyle(
-                                    fontSize: 16*textScaleFactor,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
                         ),
                       ),
                       InkWell(
